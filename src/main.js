a4676b6085fb888c4cddb8c758a26830840161ca
leonardo a <leonardo.akato@gmail.com>
2018-07-14 14:13:12 -0300

    axios setup

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import '@babel/polyfill'
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+ 
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
