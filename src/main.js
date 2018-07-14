8d667dddb0bfccbdbad56379e5713d7a593e0c6f
leonardo a <leonardo.akato@gmail.com>
2018-07-14 14:29:10 -0300

    add custom purplish/yellowish theme

--- src/main.js
+++ src/main.js
@@ -10,6 +10,18 @@ import VueRouter from 'vue-router'
 Vue.use(VueAxios, axios)
 Vue.use(Vuex)
 Vue.use(VueRouter)
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#691b99',
+    primaryl: '#9b4dcb',
+    primaryd: '#37006a',
+    secondary: '#fdd835',
+    secondaryl: '#ffff6b',
+    secondaryd: '#c6a700',
+    accent: '#8c9eff',
+    error: '#b71c1c'
+  }
+})
 
 Vue.config.productionTip = false
 
