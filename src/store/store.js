a4702736f97d077887ab6f3542b221b1b8b915fd
leonardo a <leonardo.akato@gmail.com>
2018-07-15 17:51:18 -0300

    add filtered getters placeholders

--- src/store/store.js
+++ src/store/store.js
@@ -33,6 +33,10 @@ export const store = new Vuex.Store({
           align: 'left',
           value: 'availability'}
       ],
+      filters:{
+          rating: 4.9,
+          type: 'Cages'
+        },
       products:[
         {
           name: 'Living World Deluxe Habitat, Large',
@@ -55,13 +59,34 @@ export const store = new Vuex.Store({
     user: state => {
       return state.user;
     },
-    headers: state =>{
+    headers: state => {
       console.log (state.ProductListing.headers)
       return state.ProductListing.headers;
     },
-    products: state =>{
-      console.log (state.ProductListing.products)
-      return state.ProductListing.products;
+    filters: state => {
+      return state.ProductListing.filters;
+    },
+    filteredProducts: state => {
+      let filters = state.ProductListing.filters;
+      console.log('filters')
+      console.log(filters)
+      // let filteredProducts = [];
+      let products = state.ProductListing.products;
+      console.log('prods')
+      console.log(products)
+      // for (var key in filters) {
+      //   if (!filters.hasOwnProperty(key)) continue;
+      //   var obj = filters[key];
+      //   for (var prop in obj) {
+      //     if(!obj.hasOwnProperty(prop)) continue;
+      //     filteredProducts.push(obj[prop])
+      //   }
+      // }
+      console.log('filtered prod')
+
+      let filteredProducts = products.filter((e) => filters.includes(e.id));
+      console.log(filteredProducts);
+      return products;
     }
   },
   mutations: {
