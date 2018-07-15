20fcf0c76a84883366fc1bd92c96030df1dcd922
leonardo a <leonardo.akato@gmail.com>
2018-07-15 01:25:48 -0300

    set user storage getters and setters

--- src/store/store.js
+++ src/store/store.js
@@ -4,11 +4,30 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 export const store = new Vuex.Store({
   state: {
+    user:{
+      id: 0,
+      name: '',
+    },
   },
   getters: {
+    getUser: state => {
+      return state.user;
+    },
   },
   mutations: {
+    changeUserid(state, payload){
+      state.user.id = payload.id
+    },
+    changeUsername(state, payload){
+      state.user.name = payload.username
+    },
   },
   actions: {
+    changeUserid (context, payload) {
+      context.commit('changeUserid',payload)
+    },
+    changeUsername (context, payload) {
+      context.commit('changeUsername',payload)
+    },
   }
 });
