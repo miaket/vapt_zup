561a9debeb4308d0ead3c415c16999bb13588113
leonardo a <leonardo.akato@gmail.com>
2018-07-14 23:59:25 -0300

    remove drawer variables

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,6 @@
     <v-content>
       <hello-world/>
     </v-content>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile @click="right = !right">
-          <v-list-tile-action>
-            <v-icon>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer :fixed="fixed" app>
       <span>&copy; 2017</span>
     </v-footer>
@@ -28,7 +12,7 @@
 
 <script>
 import HelloWorld from './components/HelloWorld'
-import Toolbar from './components/Shared/Toolbar.vue'
+import Toolbar from './components/Toolbar.vue'
 
 export default {
   name: 'App',
@@ -38,17 +22,11 @@ export default {
   },
   data () {
     return {
-      clipped: false,
-      drawer: true,
-      fixed: false,
       items: [{
         icon: 'bubble_chart',
         title: 'Inspire'
       }],
-      miniVariant: false,
       right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
     }
   }
 }
