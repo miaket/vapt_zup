e6a0f27e1a14f72d6899d10250870bb70659f29d
leonardo a <leonardo.akato@gmail.com>
2018-07-15 05:54:38 -0300

    lint

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -4,20 +4,20 @@
     class="primary"
     dark
   >
-  <v-toolbar-title>List filter</v-toolbar-title>
-  <v-spacer></v-spacer>
-  <v-btn 
-    @click.stop="signOut"
-    class="primaryd">
-    sign out
-  </v-btn>
+    <v-toolbar-title>List filter</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn 
+      @click.stop="signOut"
+      class="primaryd">
+      sign out
+    </v-btn>
   </v-toolbar>
 </template>
 
 <script>
   export default {
     data(){
-
+      return{}
     },
     methods: {
       signOut() {
