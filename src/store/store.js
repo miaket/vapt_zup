631b423c2da4ea6b4c4f424a9efaf10803071eee
leonardo a <leonardo.akato@gmail.com>
2018-07-15 05:08:00 -0300

    add header store - data/getters

--- src/store/store.js
+++ src/store/store.js
@@ -8,11 +8,28 @@ export const store = new Vuex.Store({
       id: 0,
       name: '',
     },
+    headers: [
+      {
+        text: 'Dessert (100g serving)',
+        align: 'left',
+        sortable: false,
+        value: 'name'
+      },
+      { text: 'Calories', value: 'calories' },
+      { text: 'Fat (g)', value: 'fat' },
+      { text: 'Carbs (g)', value: 'carbs' },
+      { text: 'Protein (g)', value: 'protein' },
+      { text: 'Iron (%)', value: 'iron' }
+    ],
   },
   getters: {
     getUser: state => {
       return state.user;
     },
+    getHeaders: state =>{
+      console.log (state.headers)
+      return state.headers;
+    }
   },
   mutations: {
     changeUserid(state, payload){
