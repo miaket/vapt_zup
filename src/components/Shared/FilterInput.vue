d49e9d0672709dae08e2859889f4d32045149d05
leonardo a <leonardo.akato@gmail.com>
2018-07-15 23:00:20 -0300

    filter inputs placeholder

--- src/components/Shared/FilterInput.vue
+++ src/components/Shared/FilterInput.vue
@@ -1,13 +1,27 @@
 <template>
-  <v-container fluid>
-    
+<v-container grid-list-md text-xs-center>
+  <v-layout row wrap>
+    <v-flex sm3  v-for="(header, index) in headers" :key=index>
+        {{header.value}}
+      <v-text-field>
+      </v-text-field>
+    </v-flex>
+  
+  </v-layout>
   </v-container>
 </template>
 
 <script>
-export default {
-  
-}
+  export default {
+    data(){
+      return{
+        headers: this.$store.getters.headers,
+        products: this.$store.getters.filteredProducts,
+        search:'',
+        filter:false
+      }
+    },
+  }
 </script>
 
 <style scoped>
