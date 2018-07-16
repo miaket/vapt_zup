3773cb58c3f518686ab323dda5308ed3d0c523c5
leonardo a <leonardo.akato@gmail.com>
2018-07-16 14:50:16 -0300

    computed variables + axios

--- src/components/FilterApp.vue
+++ src/components/FilterApp.vue
@@ -28,7 +28,7 @@
     <v-chip v-if="updateProductsAvailability">{{updateProductsAvailability}}</v-chip>
     
     <v-data-table
-      :headers="headers"
+      :headers="computedProducts"
       :items="searchedProducts"
       hide-actions
       class="elevation-1">
@@ -80,6 +80,11 @@ import axios from 'axios'
       }
     },
     computed: {
+      computedProducts(){
+        this.headers = this.$store.getters.headers;
+        this.products = this.$store.getters.filteredProducts;
+        return this.$store.getters.headers;
+      },
       searchedProducts: function () {
         let searchList;
         //to-do fix this weird call
@@ -104,12 +109,88 @@ import axios from 'axios'
       }
     },
     mounted () {
-      axios
-        .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      let axios = require('axios');
+      let MockAdapter = require('axios-mock-adapter');
+      // This sets the mock adapter on the default instance
+      let mock = new MockAdapter(axios);
+
+      // Mock GET request to /users when param `searchText` is 'John' 
+      // arguments for reply are (status, data, headers)
+      mock.onGet('/productlist').reply(200, {
+        productListing:{
+          headers: [
+            { 
+              text: 'Product name',
+              align: 'left',
+              value: 'name'
+            },
+            {
+              text: 'Pet Category',
+              align: 'left',
+              value: 'category'
+            },
+            { 
+              text: 'Type',
+              align: 'left',
+              value: 'type'
+            },
+            { 
+              text: 'Price',
+              value: 'price'
+            },
+            { 
+              text: 'Rating',
+              value: 'rating'
+            },
+            { 
+              text: 'Availability',
+              align: 'left',
+              value: 'availability'
+            }
+          ],
+          products:[
+            {
+              name: 'Living World Deluxe Habitat, Large',
+              category: 'Small animals',
+              type: 'Cages',
+              price: 50.00,
+              rating: 4.5,
+              availability: 'Out of Stock'
+            },
+            {
+              name: 'Prevue Pet Products 528 Universal Small Animal Home, Dark Gray',
+              category: 'Small animals',
+              type: 'Cages',
+              price: 66.99,
+              rating: 4.9,
+              availability: 'In Stock'
+            },
+            {
+              name: 'Kaytee Timothy Complete Diet for Rabbit',
+              category: 'Small animals',
+              type: 'Foods',
+              price: 11.49,
+              rating: 4.7,
+              availability: 'In Stock'
+            },
+            {
+              name: 'Purina Fancy Feasts Grilled Seafood Collection ',
+              category: 'Cats',
+              type: 'Foods',
+              price: 12.59,
+              rating: 4.4,
+              availability: 'In Stock'
+            },
+          ]
+        }
+      });
+      let auxresponse
+      axios.get('/productlist')
         .then(response => {
-          console.log(response)
-          this.info = response
-          });
+          console.log('respds')
+          console.log(response.data.productListing);
+          this.$store.dispatch('changeProductListing', response.data.productListing)
+        });
     }
   }
 </script>
